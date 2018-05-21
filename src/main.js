42eb905f5332addb876e65b00fa32246f3c7ee1a
小五 <wu.vkcyan@gmail.com>
2018-05-21 22:33:26 +0800

    webpack解决跨域问题 完成登录拦截器

--- src/main.js
+++ src/main.js
@@ -4,9 +4,48 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 Vue.use(Element)
+Vue.prototype.$http = axios   //引入axios
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  let token = sessionStorage.getItem('demo-token')
+  if (token !== 'null' && token !== null) {
+    if (to.path == '/todolist'){
+      next()
+    }else {
+      console.log('存在token 进入系统')
+      next('/todolist')
+    }
+  } else {
+    if (to.path == '/todolist') {
+      console.log('token不存在 跳转主页面');
+      next('/')
+    } else {
+      console.log('跳转主页面');
+      next()
+    }
+  }
+  // if(to.path == '/') {
+  //   console.log('存在token 进入系统')
+  //   if (token !== 'null' && token !== null) {
+  //     next('/todoList')
+  //   }
+  //   next()
+  //   console.log('不存在token 跳转回登录页面')
+  // } else {
+  //   if (token !== 'unll' && token !== null) {
+  //     console.log('token存在 正常跳转')
+  //     next()
+  //   }else {
+  //     next('/')
+  //     console.log('token不存在 跳转回登录页面')
+  //   }
+  // }
+})
+
 
 /* eslint-disable no-new */
 new Vue({
